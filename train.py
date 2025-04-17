e83710f0a4b21797a38228e0e87a211e9e0c4bd3
Billy-Bryan <151909896+QingbinZou@users.noreply.github.com>
2025-04-17 22:30:14 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import os
 
 # 数据路径
-rs_path = 'data/rolling'
-gs_path = 'data/global'
+rs_path = 'data_train/RS'
+gs_path = 'data_train/GS'
 
 # 数据加载（你可以设置 augment=True 在 dataset.py 中开启模糊增强）
 train_gen = RSDataset(rs_path, gs_path, batch_size=4)
@@ -21,11 +21,13 @@ def combined_loss(y_true, y_pred):
     l1 = tf.reduce_mean(tf.abs(y_true - y_pred))
     ssim_loss = 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, max_val=1.0))
 
-    sobel_x = tf.image.sobel_edges(y_pred)[..., 0]
-    sobel_y = tf.image.sobel_edges(y_pred)[..., 1]
+    sobel = tf.image.sobel_edges(y_pred)  # [B, H, W, C, 2]
+    sobel_x = sobel[..., 0]
+    sobel_y = sobel[..., 1]
     grad = tf.reduce_mean(tf.abs(sobel_x) + tf.abs(sobel_y))
 
     return 0.6 * l1 + 0.2 * ssim_loss + 0.2 * grad
+
 
 
 # 编译模型
@@ -35,7 +37,7 @@ model.compile(optimizer='adam', loss=combined_loss)
 checkpoint = ModelCheckpoint("best_model.h5", monitor='loss', save_best_only=True)
 
 # 开始训练
-history = model.fit(train_gen, epochs=300, callbacks=[checkpoint])
+history = model.fit(train_gen, epochs=100, callbacks=[checkpoint])
 
 # 绘制 loss 曲线图
 plt.plot(history.history["loss"], label="Training Loss")
