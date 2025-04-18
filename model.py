fdf36dc40884a086b357c599ef0c32cfb62c9035
Billy-Bryan <151909896+QingbinZou@users.noreply.github.com>
2025-04-18 03:38:25 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,17 +1,14 @@
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Concatenate, Add, Dropout
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Concatenate, Add, Dropout, Resizing
 from tensorflow.keras.models import Model
 
-# 残差模块
 def res_block(x, filters, kernel_size=3):
     shortcut = x
     x = Conv2D(filters, kernel_size, activation='relu', padding='same')(x)
-    x = Dropout(0.1)(x)
+    x = Dropout(0.3)(x)
     x = Conv2D(filters, kernel_size, padding='same')(x)
-    x = Add()([x, shortcut])
-    return x
+    return Add()([x, shortcut])
 
-# 构建 ResUNet with Conv2DTranspose 上采样
-def build_resunet(input_shape=(384, 384, 3)):
+def build_resunet(input_shape=(384, 512, 3)):
     inputs = Input(shape=input_shape)
 
     # Encoder
@@ -32,20 +29,24 @@ def build_resunet(input_shape=(384, 384, 3)):
     c4 = res_block(c4, 256)
 
     # Decoder
-    u5 = Conv2DTranspose(128, 2, strides=2, padding='same')(c4)
+    u5 = UpSampling2D()(c4)
+    u5 = Conv2D(128, 3, activation='relu', padding='same')(u5)
+    u5 = Resizing(96, 128)(u5)
     m5 = Concatenate()([u5, c3])
-    c5 = Conv2D(128, 3, activation='relu', padding='same')(m5)
-    c5 = res_block(c5, 128)
+    c5 = res_block(m5, 256)
 
-    u6 = Conv2DTranspose(64, 2, strides=2, padding='same')(c5)
+    u6 = UpSampling2D()(c5)
+    u6 = Conv2D(64, 3, activation='relu', padding='same')(u6)
+    u6 = Resizing(192, 256)(u6)
     m6 = Concatenate()([u6, c2])
-    c6 = Conv2D(64, 3, activation='relu', padding='same')(m6)
-    c6 = res_block(c6, 64)
+    c6 = res_block(m6, 128)
 
-    u7 = Conv2DTranspose(32, 2, strides=2, padding='same')(c6)
+    u7 = UpSampling2D()(c6)
+    u7 = Conv2D(32, 3, activation='relu', padding='same')(u7)
+    u7 = Resizing(384, 512)(u7)
     m7 = Concatenate()([u7, c1])
-    c7 = Conv2D(32, 3, activation='relu', padding='same')(m7)
-    c7 = res_block(c7, 32)
+    c7 = res_block(m7, 64)
 
-    outputs = Conv2D(3, (1,1), activation='sigmoid', padding='same')(c7)
+    outputs = Conv2D(3, 1, activation='sigmoid', padding='same')(c7)
+
     return Model(inputs, outputs)
